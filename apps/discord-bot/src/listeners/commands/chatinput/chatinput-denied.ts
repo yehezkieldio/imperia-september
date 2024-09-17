390ede18bf38e3f0db3bf24997a9526b0eed215e
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-09-17 10:36:10 +0800

    refactor: clean chat input command listeners

--- apps/discord-bot/src/listeners/commands/chatinput/chatinput-denied.ts
+++ apps/discord-bot/src/listeners/commands/chatinput/chatinput-denied.ts
@@ -1,5 +1,4 @@
 import { type ChatInputCommandDeniedPayload, Listener, type UserError } from "@sapphire/framework";
-import { resolveKey } from "@sapphire/plugin-i18next";
 import { ImperiaEvents } from "#lib/extensions/constants/events";
 
 export class ChatInputCommadDeniedListener extends Listener {
@@ -12,24 +11,22 @@ export class ChatInputCommadDeniedListener extends Listener {
     }
 
     public async run(error: UserError, payload: ChatInputCommandDeniedPayload) {
-        const { logger, services, utilities } = this.container;
         const { interaction } = payload;
-
-        logger.debug(`ChatInputCommadDeniedListener: ${error.identifier}`);
-
         if (!interaction.guildId) return;
 
-        const response: string = await services.response.commandDenied(interaction.guildId, error);
+        this.container.logger.debug(`ChatInputCommadDeniedListener: ${error.identifier}`);
+
+        const response: string = await this.container.services.response.commandDenied(interaction.guildId, error);
 
         if (interaction.deferred || interaction.replied) {
             return interaction.editReply({
-                content: utilities.bot.isATranslationKey(response) ? await resolveKey(interaction, response) : response,
+                content: await this.container.utilities.bot.getResponse(response, interaction),
                 allowedMentions: { users: [interaction.user.id], roles: [] },
             });
         }
 
         return interaction.reply({
-            content: utilities.bot.isATranslationKey(response) ? await resolveKey(interaction, response) : response,
+            content: await this.container.utilities.bot.getResponse(response, interaction),
             allowedMentions: { users: [interaction.user.id], roles: [] },
             ephemeral: true,
         });
--- apps/discord-bot/src/listeners/commands/chatinput/chatinput-error.ts
+++ apps/discord-bot/src/listeners/commands/chatinput/chatinput-error.ts
@@ -1,4 +1,4 @@
-import { type ChatInputCommandErrorPayload, Listener, type UserError } from "@sapphire/framework";
+import { ArgumentError, type ChatInputCommandErrorPayload, Listener, UserError } from "@sapphire/framework";
 import { ImperiaEvents } from "#lib/extensions/constants/events";
 
 export class ChatInputCommadErrorListener extends Listener {
@@ -10,14 +10,29 @@ export class ChatInputCommadErrorListener extends Listener {
         });
     }
 
-    public async run(error: UserError, payload: ChatInputCommandErrorPayload) {
-        const { logger } = this.container;
+    public async run(error: Error, payload: ChatInputCommandErrorPayload) {
         const { interaction } = payload;
+        if (!interaction.guildId) return;
 
-        logger.debug(`ChatInputCommadErrorListener: ${error.identifier}`);
+        if (error instanceof UserError) {
+            this.container.logger.debug(`ChatInputCommadErrorListener: UserError ${error.identifier}`);
+        }
+
+        if (error instanceof ArgumentError) {
+            this.container.logger.debug(`ChatInputCommadErrorListener: ArgumentError ${error.identifier}`);
+        }
+
+        const response: string = await this.container.services.response.commandError(interaction.guildId, error);
+
+        if (interaction.deferred || interaction.replied) {
+            return interaction.editReply({
+                content: await this.container.utilities.bot.getResponse(response, interaction),
+            });
+        }
 
         return interaction.reply({
-            content: `${error.identifier}\n${error.message}`,
+            content: await this.container.utilities.bot.getResponse(response, interaction),
+            ephemeral: true,
         });
     }
 }
