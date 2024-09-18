c2083384acc073215dd631174e83656df36b2ca7
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-09-18 16:49:31 +0800

    chore: header

--- apps/discord-bot/src/listeners/client/client-ready.ts
+++ apps/discord-bot/src/listeners/client/client-ready.ts
@@ -15,5 +15,27 @@ export class ClientReadyListener extends Listener {
     public async run(client: Client): Promise<void> {
         const { username, id } = client.user as ClientUser;
         this.container.logger.info(`ClientReadyListener: Successfully logged in as ${username} (${id})`);
+
+        await Bun.sleep(3000);
+
+        this.header();
+    }
+
+    public async header() {
+        console.log(
+            `
++---------------------------------------------------------------------+
+|ooooo                                                  o8o           |
+|'888'                                                  '"'           |
+| 888  ooo. .oo.  .oo.   oo.ooooo.   .ooooo.  oooo d8b oooo   .oooo.  |
+| 888  '888P"Y88bP"Y88b   888' '88b d88' '88b '888""8P '888  'P  )88b |
+| 888   888   888   888   888   888 888ooo888  888      888   .oP"888 |
+| 888   888   888   888   888   888 888    .o  888      888  d8(  888 |
+|o888o o888o o888o o888o  888bod8P' 'Y8bod8P' d888b    o888o 'Y888""8o|
+|                         888                                         |
+|                        o888o                                        |
++---------------------------------------------------------------------+
+            `,
+        );
     }
 }
